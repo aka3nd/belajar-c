478d0e82968c7155621a3f85b6b3e73c85e70347
aka3nd <fileaing1@gmail.com>
2026-03-23 21:47:28 +0700

    add basic

---
+++ basic/ifelse/ifelse1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int a = 2;
+  if (a == 2) {
+    printf("ya ini angka 2\n");
+  } else if (a == 6) {
+    printf("ini angka 6\n");
+  } else {
+    printf("bukan angka yang dicari\n");
+  }
+  return 0;
+}
