0477cf6f0d9f84c8250ee65e26c73e0056279354
Aka3nd <fileaing1@gmail.com>
2026-04-01 09:50:31 +0700

    add constanta

---
+++ basic/constan/const1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  const int myAngka = 5; // const adalah variable yang tidakbisa
+  // diubah dan hanya bisa dibaca
+  printf("myAngka %d\n", myAngka);
+  return 0;
+}
